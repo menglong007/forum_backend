8030757f022a9c1ecaf9672cdb0a06b5c9f7c43f
Your Name <you@example.com>
2025-01-07 14:03:19 +0700

    myapp

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9337a9c71abc4a0e400d0c4d9c1865c74f0bb277")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa6b566931747f563b6a1e460be30e3a2c71f7d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
