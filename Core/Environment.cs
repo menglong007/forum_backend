e26c5e18b2d1090402890fc0949f590415d26ed7
Your Name <you@example.com>
2025-01-07 12:09:03 +0700

    fix

--- Core/Environment.cs
+++ Core/Environment.cs
@@ -17,7 +17,7 @@ public static class AppEnvironment
     public static string DateFormat => Environment.GetEnvironmentVariable("DATE_FORMAT") ?? "dd/MM/yyyy";
 
     public static string DbConnection => Environment.GetEnvironmentVariable("DB_CONNECTION") ??
-                                            "Server=localhost:5433;Database=test_db;User Id=admin;Password=admin123;";
+                                            "Server=wickedly-sheltering-chipmunk.data-1.apse1.tembo.io;Port:5432;Database=test_db;User Id=postgres;Password=cPnw6LZ4smh3POBf;";
 
     public static string JwtIssuer => Environment.GetEnvironmentVariable("JWT_ISSUER") ?? "https://dev.eventhub.one";
 
--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3b90a31e7c925544819078cf295c6d6675514a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
